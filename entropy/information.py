665b695d06e3e5380a8886eb8de71de738f6de02
ddfabbro <fabda01@gmail.com>
2017-12-26 19:43:22 -0200

    Update functions

--- entropy/information.py
+++ entropy/information.py
@@ -7,28 +7,35 @@ Created on Tue Oct 24 09:39:41 2017
 """
 import numpy as np
 
-def information_xy(x,y,bins):
-    """
-    Calculates:
-    ---Marginal Entropy H(X)
-    ---Marginal Entropy H(Y)
-    ---Joint Entropy H(X,Y)
-    ---Mutual Information I(X,Y)
-    ---Conditional Entropy H(X|Y)
-    ---Conditional Entropy H(Y|X)
-   
-    In: numpy.array (1D), numpy.array(1D), int
-    Out: (float, float, float, float, float, float) 
-    """
+def marginalEntropy(x,bins):
     px = np.histogram(x,bins)[0]/float(np.sum(np.histogram(x,bins)[0]))
-    py = np.histogram(y,bins)[0]/float(np.sum(np.histogram(y,bins)[0]))
+    return np.nansum(px*np.log(1/px))
+
+def jointEntropy(x,y,bins):
     pxy = np.histogram2d(x,y,bins)[0]/float(np.sum(np.histogram2d(x,y,bins)[0]))
-   
-    hx = np.nansum(px*np.log(1/px))
-    hy = np.nansum(py*np.log(1/py))
-    hxy = np.nansum(pxy*np.log(1/pxy))
-    ixy =  hy+hx-hxy
-    hx_y = hxy-hy
-    hy_x = hxy-hx
-   
-    return (hx,hy,hxy,ixy,hx_y,hy_x)
+    return np.nansum(pxy*np.log(1/pxy))
+
+def mutualInformation(x,y,bins):
+    hx = marginalEntropy(x,bins)
+    hy = marginalEntropy(y,bins)
+    hxy = jointEntropy(x,y,bins)
+    return hx+hy-hxy
+
+def conditionalEntropy(x,y,bins):
+    hx = marginalEntropy(x,bins)
+    hy = marginalEntropy(y,bins)
+    hxy = jointEntropy(x,y,bins)
+    return {'H(X|Y)': hxy-hy,
+            'H(Y|X)': hxy-hx}
+
+if __name__ == "__main__":
+    x = np.random.normal(0, 1, 1000)
+    y = np.random.normal(0, 1, 1000)
+    bins = 20
+    
+    print 'H(X) is ' + str(marginalEntropy(x,bins))
+    print 'H(Y) is ' + str(marginalEntropy(y,bins))
+    print 'H(X,Y) is ' + str(jointEntropy(x,y,bins))
+    print 'I(X,Y) is ' + str(mutualInformation(x,y,bins))
+    print 'H(X|Y) is ' + str(conditionalEntropy(x,y,bins)['H(X|Y)'])
+    print 'H(Y|X) is ' + str(conditionalEntropy(x,y,bins)['H(Y|X)'])
