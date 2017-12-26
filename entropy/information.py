858db3c197a47330df6d9408f2dd51007831cf24
Davi Araujo Dal Fabbro <fabda01@gmail.com>
2017-12-26 20:14:45 -0200

    Update information.py

--- entropy/information.py
+++ entropy/information.py
@@ -10,24 +10,20 @@ import numpy as np
 np.seterr(divide='ignore', invalid='ignore')
 
 def marginalEntropy(x,bins):
-    np.seterr(divide='ignore', invalid='ignore')
     px = np.histogram(x,bins)[0]/float(np.sum(np.histogram(x,bins)[0]))
     return np.nansum(px*np.log(1/px))
 
 def jointEntropy(x,y,bins):
-    np.seterr(divide='ignore', invalid='ignore')
     pxy = np.histogram2d(x,y,bins)[0]/float(np.sum(np.histogram2d(x,y,bins)[0]))
     return np.nansum(pxy*np.log(1/pxy))
 
 def mutualInformation(x,y,bins):
-    np.seterr(divide='ignore', invalid='ignore')
     hx = marginalEntropy(x,bins)
     hy = marginalEntropy(y,bins)
     hxy = jointEntropy(x,y,bins)
     return hx+hy-hxy
 
 def conditionalEntropy(x,y,bins):
-    np.seterr(divide='ignore', invalid='ignore')
     hx = marginalEntropy(x,bins)
     hy = marginalEntropy(y,bins)
     hxy = jointEntropy(x,y,bins)
